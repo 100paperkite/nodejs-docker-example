9494abf8063bfa4f67e5d435793e781631f9415b
Baek Jiyeon <100.paperkite@gmail.com>
2023-05-03 21:03:15 +0900

    docker-compose로 redis와 서버 같이 띄우기

--- server/app.js
+++ server/app.js
@@ -1,13 +1,25 @@
 import express from 'express';
+import { createClient } from 'redis';
 
-const PORT = 10000;
-const HOST = '0.0.0.0';
+const { env } = process;
 
-const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World');
+const KEY_NAME = 'number';
+
+const client = createClient({
+  url: `redis://${env.REDIS_HOST}:${env.REDIS_PORT}`,
 });
 
-app.listen(PORT, HOST, () => {
-  console.log(`Running on http://${HOST}:${PORT}`);
+await client.connect();
+await client.set(KEY_NAME, 0);
+
+const app = express();
+app.get('/', async (req, res) => {
+  const number = await client.get(KEY_NAME);
+  await client.set(KEY_NAME, parseInt(number) + 1);
+
+  res.send(`숫자가 1만큼씩 올라갑니다: ${number}`);
+});
+
+app.listen(env.PORT, env.HOST, () => {
+  console.log(`Running on http://${env.HOST}:${env.PORT}`);
 });
